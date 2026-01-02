1921260310a21343d5ecfafceaeb85cee2bff1e0
YKDBontekoe <youribontekoe@gmail.com>
2026-01-02 14:27:40 +0000

    Deploying to gh-pages from @ YKDBontekoe/ai-book-world-builder@14347f84c60fde9029392bdf352b71b24f3437f0 🚀

--- assets/formatter-QJ4M4OGQ-BLbhl3hR.js
+++
@@ -1,1 +0,0 @@
-import{d as m,r as o}from"./DocsRenderer-GHJI37HO-CkGfWaf_.js";import{Z as t}from"./iframe-CTrsbqk6.js";import"./preload-helper-PPVm8Dsz.js";import"./react-18-CskSuaWq.js";import"./index-D4Fw3ANf.js";import"./jsx-runtime-BzYutUg-.js";import"./index-CWS9GDqz.js";var e=t(o(),1),l=(0,e.default)(2)(async(i,r)=>i===!1?r:m(r));export{l as formatter};
---
+++ assets/tooltip.stories-CWwsQd8A.js
@@ -0,0 +1,10 @@
+import{j as o}from"./jsx-runtime-SWltMPTP.js";import{B as e}from"./button-CSmJDwgs.js";import{T as i,c as p,a as n,b as s}from"./tooltip-CGMSB50Q.js";import"./iframe-CacMBq-9.js";import"./preload-helper-PPVm8Dsz.js";import"./index-C1BImRFM.js";import"./index-B_jtOnfb.js";import"./utils-CDN07tui.js";import"./index-Dc_FVRD7.js";import"./index-LO9Q5oNG.js";import"./index-f2Zd03F7.js";import"./index-CsEtRIui.js";import"./index-Cak3uSYQ.js";import"./index-uLCiVs5G.js";import"./index-aHuYAUaf.js";import"./index-CCfa07NI.js";import"./index-CjeIGFCl.js";import"./index-CvnDULNU.js";import"./index-Bmq28RQK.js";import"./index-BLKrnZlb.js";import"./index-DRZdwNHo.js";const S={title:"Design System/Atoms/Tooltip",component:i,tags:["autodocs"],decorators:[r=>o.jsx(p,{children:o.jsx(r,{})})]},t={render:r=>o.jsxs(i,{...r,children:[o.jsx(n,{asChild:!0,children:o.jsx(e,{variant:"outline",children:"Hover"})}),o.jsx(s,{children:o.jsx("p",{children:"Add to library"})})]})};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  render: (args: any) => <Tooltip {...args}>
+            <TooltipTrigger asChild>
+                <Button variant="outline">Hover</Button>
+            </TooltipTrigger>
+            <TooltipContent>
+                <p>Add to library</p>
+            </TooltipContent>
+        </Tooltip>
+}`,...t.parameters?.docs?.source}}};const _=["Default"];export{t as Default,_ as __namedExportsOrder,S as default};
