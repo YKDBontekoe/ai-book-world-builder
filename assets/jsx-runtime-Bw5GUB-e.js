8128ae231dbb82c27c391022a5d53a81a4de2a99
YKDBontekoe <youribontekoe@gmail.com>
2026-01-08 19:11:51 +0000

    Deploying to gh-pages from @ YKDBontekoe/ai-book-world-builder@f1842eb0c42c70a06e3b660f52b8ec96c7168f14 🚀

---
+++ assets/jsx-runtime-Bw5GUB-e.js
@@ -0,0 +1,1 @@
+import{a as r}from"./iframe-Cb3ptgCd.js";var s=r();export{s as j};
---
+++ assets/select.stories-kBeLp-nI.js
@@ -0,0 +1,17 @@
+import{j as e}from"./jsx-runtime-Bw5GUB-e.js";import{S as l,a as p,b as c,c as n,d as o,e as s,f as t}from"./select-C19uoyQB.js";import"./iframe-Cb3ptgCd.js";import"./preload-helper-PPVm8Dsz.js";import"./utils-CDN07tui.js";import"./chevron-down-CrHCvaC4.js";import"./createLucideIcon-DkymiNbc.js";import"./check-BJNak-ax.js";import"./index-Cdk6nCdW.js";import"./index-BdQq_4o_.js";import"./index-Dc_FVRD7.js";import"./index-Dl9cR6Ul.js";import"./index-B8CNdpQ2.js";import"./index-DHXEaNoz.js";import"./index-DDscbscV.js";import"./index-Cz3dlpV3.js";import"./index-1dqkAWnV.js";import"./index-BgCrHvZZ.js";import"./index-CToZUhCE.js";import"./index-CO-LdhfU.js";import"./index-QgRIytkw.js";import"./index-V_zvZqVa.js";import"./index-bEmXfr4_.js";import"./index-BrKGXCjs.js";import"./index-DGrBS4g_.js";import"./index-CcayX9og.js";const V={title:"Design System/Atoms/Select",component:l,tags:["autodocs"]},r={render:a=>e.jsxs(l,{...a,children:[e.jsx(p,{className:"w-[180px]",children:e.jsx(c,{placeholder:"Select a fruit"})}),e.jsx(n,{children:e.jsxs(o,{children:[e.jsx(s,{children:"Fruits"}),e.jsx(t,{value:"apple",children:"Apple"}),e.jsx(t,{value:"banana",children:"Banana"}),e.jsx(t,{value:"blueberry",children:"Blueberry"}),e.jsx(t,{value:"grapes",children:"Grapes"}),e.jsx(t,{value:"pineapple",children:"Pineapple"})]})})]})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: (args: any) => <Select {...args}>
+            <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Select a fruit" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectGroup>
+                    <SelectLabel>Fruits</SelectLabel>
+                    <SelectItem value="apple">Apple</SelectItem>
+                    <SelectItem value="banana">Banana</SelectItem>
+                    <SelectItem value="blueberry">Blueberry</SelectItem>
+                    <SelectItem value="grapes">Grapes</SelectItem>
+                    <SelectItem value="pineapple">Pineapple</SelectItem>
+                </SelectGroup>
+            </SelectContent>
+        </Select>
+}`,...r.parameters?.docs?.source}}};const _=["Default"];export{r as Default,_ as __namedExportsOrder,V as default};
