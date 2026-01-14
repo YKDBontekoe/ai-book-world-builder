1fa17e969039b67349c8cf4dd1effc7ea1b5d1ec
YKDBontekoe <youribontekoe@gmail.com>
2026-01-14 19:21:17 +0000

    Deploying to gh-pages from @ YKDBontekoe/ai-book-world-builder@5b0efba1b1ec969ade9f6fcc51907d19c6113efa 🚀

--- assets/index-CSiHOokW.js
+++
@@ -1,1 +0,0 @@
-import{r as o,R as s}from"./iframe-DHtrQyLs.js";import{u}from"./index-DqY5D1VG.js";var i=s[" useId ".trim().toString()]||(()=>{}),c=0;function f(r){const[t,e]=o.useState(i());return u(()=>{e(a=>a??String(c++))},[r]),t?`radix-${t}`:""}export{f as u};
--- assets/index-DqY5D1VG.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./iframe-DHtrQyLs.js";var a=globalThis?.document?t.useLayoutEffect:()=>{};export{a as u};
